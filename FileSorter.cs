4c8f48872344045843f4fa136b7232513ca9df62
Nikita Sarkisov <s.nikita@outlook.com>
2021-06-19 21:59:59 +0300

    Add project files.

---
+++ FileSorter.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LargeFileSorter
+{
+    internal class FileSorter
+    {
+        private FileStream source;
+        private FileStream map;
+        private readonly Encoding encoding;
+
+        public FileSorter(Encoding encoding)
+        {
+            this.encoding = encoding;
+        }
+
+        // Buffers are declared as fields to avoid allocating them in stack at every method call.
+        private byte[] strbuff;
+        private readonly byte[] buff1 = new byte[12];
+        private readonly byte[] buff2 = new byte[12];
+
+        /// <summary>
+        /// Sorts a large text file.
+        /// </summary>
+        /// <param name="path">Path to unsorted text file</param>
+        /// <returns>Path to sorted file.</returns>
+        public string Sort(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException($"'{nameof(path)}' cannot be null or whitespace.", nameof(path));
+            }
+
+            string fullpath = Path.GetFullPath(path);
+            string mapPath = fullpath + "_map.bin";
+            string outputfile;
+
+            try
+            {
+                source = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
+                map = new FileStream(mapPath, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
+                Console.WriteLine("-- Source file is {0} bytes", source.Length);
+
+                // Step 1
+                // Building a binary map of the source text file.
+                // The map contains a Position and Length of every line in source file.
+                // Each record in map file is 12 bytes long: int (4bytes) + long (8bytes).
+                Console.WriteLine("-- Building file map...");
+                int maxLength = BuildMap();
+                // The buffer is created to hold a line in binary form.
+                // It is set to fit the longest line.
+                // This way the program memory usage depends on LineMaxLength parameter.
+                strbuff = new byte[maxLength];
+
+                // Step 2
+                // Sorting map file.
+                // The values for caparison a read from the source file during sorting.
+                // As a  result map file is sorted. (Index-based sort).
+                // Comb sort is the sorting algorithm.
+                Console.WriteLine("-- Sorting file map...");
+                MapSort();
+
+                // Step 3
+                // Generate the sorted text file.
+                Console.WriteLine("-- Writing sorted file...");
+                outputfile = Write();
+            }
+            finally
+            {
+                source.Dispose();
+                map.Dispose();
+            }
+
+            // Deletes the map file.
+            File.Delete(map.Name);
+
+            return outputfile;
+        }
+
+        private int BuildMap()
+        {
+            byte[] NL = encoding.GetBytes(Environment.NewLine);
+
+            // Detect BOM at the start of a file
+            byte[] BOM = new byte[encoding.GetPreamble().Length];
+            int read = source.Read(BOM);
+            if (read != BOM.Length || !BOM.SequenceEqual(encoding.GetPreamble()))
+            {
+                // File does not have BOM at the beginning
+                source.Seek(0, SeekOrigin.Begin);
+            }
+            // else: Position already is set after the BOM
+
+            int Pos = 0;
+            int LineLength = 0;
+            int MaxLength = 0;
+            long LineStartPosition = source.Position; // In case of BOM will be set to 3, otherwise to 0;
+
+            while (true)
+            {
+                int b = source.ReadByte();
+                if (b == -1) break;
+
+                LineLength++;
+                if (NL[Pos] == (byte)b)
+                {
+                    Pos++;
+
+                    // Check if we found a NewLine
+                    if (Pos == NL.Length)
+                    {
+                        LineLength -= NL.Length;
+                        // Write line information to map file
+                        map.Write(BitConverter.GetBytes(LineStartPosition));
+                        map.Write(BitConverter.GetBytes(LineLength));
+
+                        // Get maximum string length
+                        if (LineLength > MaxLength) MaxLength = LineLength;
+
+                        LineStartPosition = source.Position;
+                        LineLength = 0;
+                        Pos = 0;
+                    }
+                }
+                else
+                {
+                    Pos = 0;
+                }
+
+            }
+
+            // Check if file ends without a NewLine
+            if (LineLength > 0)
+            {
+                map.Write(BitConverter.GetBytes(LineStartPosition));
+                map.Write(BitConverter.GetBytes(LineLength));
+                // The last line might be the longest
+                if (LineLength > MaxLength) MaxLength = LineLength;
+            }
+
+            map.Flush();
+            return MaxLength;
+        }
+        private void MapSort()
+        {
+            int Start = 0;                      // The algorithm sorts from the first element
+            int Stop = (int)(map.Length / 12);  // to the last element
+
+            int Shift; bool Sw;
+            int Bound;
+
+            Shift = (int)((Stop - Start) / 1.247f);
+            if (Shift <= 0) { Shift = 1; }
+
+
+            while (true)
+            {
+                Sw = false;
+                Bound = Stop - Shift;
+
+                for (int i = Start; i <= Bound; i++)
+                {
+                    if (Compare(i, i + Shift) > 0)
+                    {
+                        Swap(i, i + Shift);
+                        Sw = true;
+                    }
+                }
+
+                if (Sw == false && Shift == 1) { break; }
+                if (Shift > 1) { Shift = (int)(Shift / 1.247f); }
+            }
+        }
+        private string Write()
+        {
+            string filename = source.Name + "_sorted.txt";
+            StreamWriter writer = new StreamWriter(filename, false, encoding);
+            map.Seek(0, SeekOrigin.Begin);
+
+            int n = (int)(map.Length / 12);
+            for (int i = 0; i < n; i++)
+            {
+                map.Read(buff1, 0, 12);
+                long position = BitConverter.ToInt64(buff1, 0);
+                int strLength = BitConverter.ToInt32(buff1, 8);
+
+                source.Seek(position, SeekOrigin.Begin);
+                source.Read(strbuff, 0, strLength);
+                string line = encoding.GetString(strbuff, 0, strLength);
+
+                writer.WriteLine(line);
+            }
+
+            writer.Flush();
+            writer.Dispose();
+
+            return filename;
+        }
+        private int Compare(int pos1, int pos2)
+        {
+            // Read string from position 1
+            map.Seek(pos1 * 12, SeekOrigin.Begin);
+            map.Read(buff1, 0, 12);
+            long position = BitConverter.ToInt64(buff1, 0);
+            int strLength = BitConverter.ToInt32(buff1, 8);
+
+            source.Seek(position, SeekOrigin.Begin);
+            source.Read(strbuff, 0, strLength);
+
+            string S1 = encoding.GetString(strbuff, 0, strLength);
+
+            // Read string from position 2
+            map.Seek(pos2 * 12, SeekOrigin.Begin);
+            map.Read(buff1, 0, 12);
+            position = BitConverter.ToInt64(buff1, 0);
+            strLength = BitConverter.ToInt32(buff1, 8);
+
+            source.Seek(position, SeekOrigin.Begin);
+            source.Read(strbuff, 0, strLength);
+
+            string S2 = encoding.GetString(strbuff, 0, strLength);
+
+            return string.Compare(S1, S2, StringComparison.Ordinal);
+        }
+        private void Swap(int pos1, int pos2)
+        {
+            long P1 = pos1 * 12, P2 = pos2 * 12;
+
+            map.Seek(P1, SeekOrigin.Begin);
+            map.Read(buff1, 0, 12);
+            map.Seek(P2, SeekOrigin.Begin);
+            map.Read(buff2, 0, 12);
+
+            map.Seek(P1, SeekOrigin.Begin);
+            map.Write(buff2, 0, 12);
+            map.Seek(P2, SeekOrigin.Begin);
+            map.Write(buff1, 0, 12);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using CommandLine;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace LargeFileSorter
+{
+    internal class Program
+    {
+        private class Options
+        {
+            [Option("File", Default = "", HelpText = "File to sort. If empty file with this name will be generated")]
+            public string File { get; set; }
+
+            [Option("LineCount", Default = 1000, HelpText = "Number of line to generate")]
+            public int LineCount { get; set; }
+
+            [Option("MaxLength", Default = 50, HelpText = "Max line length")]
+            public int LineMaxLength { get; set; }
+
+            [Option("Validate", Default = false, HelpText = "Validate the sorted file")]
+            public bool Validate { get; set; }
+        }
+
+        private static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions);
+        }
+
+        private static void RunOptions(Options opts)
+        {
+            Encoding encoding;
+            opts.File = Path.GetFullPath(opts.File);
+
+            if (!File.Exists(opts.File))
+            {
+                Console.WriteLine("Generating new random text file...");
+                Console.WriteLine("Number of lines: {0}, Max line length: {1}", opts.LineCount, opts.LineMaxLength);
+                opts.File = GenerateRandomFile(opts.File, opts.LineCount, opts.LineMaxLength, out encoding);
+                Console.WriteLine("Generated {0}", opts.File);
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("Using file {0}", opts.File);
+                encoding = GetEncoding(opts.File);
+            }
+
+            Console.WriteLine("Sorting file {0}...", opts.File);
+            FileSorter largeFileSorter = new FileSorter(encoding);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            string sorted = largeFileSorter.Sort(opts.File);
+            stopwatch.Stop();
+            Console.WriteLine("Done.");
+            Console.WriteLine("Sorted file at: {0}", sorted);
+            Console.WriteLine("Sorting took {0}ms", stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("");
+
+            if (opts.Validate)
+            {
+                Console.WriteLine("Validating {0}...", sorted);
+                bool valid = SortedFileChecker(sorted);
+                Console.WriteLine("Done.");
+                if (valid) Console.WriteLine("File is valid!");
+                else Console.WriteLine("File is not valid!");
+                Console.WriteLine("");
+            }
+        }
+
+        public static Encoding GetEncoding(string path)
+        {
+            using (var reader = new StreamReader(path))
+            {
+                reader.Read();
+                return reader.CurrentEncoding;
+            }
+        }
+
+        public static string GenerateRandomFile(string path, int lineCount, int lineMaxLenght, out Encoding encoding)
+        {
+            Random rnd = new Random();
+            const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+            if (string.IsNullOrEmpty(path))
+            {
+                path = Path.GetFullPath(DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString()) + ".txt";
+            }
+
+            using (var writer = new StreamWriter(path))
+            {
+                StringBuilder builder = new StringBuilder(lineMaxLenght);
+
+                for (int i = 0; i < lineCount; i++)
+                {
+                    int lineLength = rnd.Next(1, lineMaxLenght);
+                    for (int j = 0; j < lineLength; j++)
+                    {
+                        builder.Append(chars[rnd.Next(0, chars.Length)]);
+                    }
+                    writer.WriteLine(builder.ToString());
+                    builder.Clear();
+                }
+
+                encoding = writer.Encoding;
+            }
+
+            return path;
+        }
+
+        public static bool SortedFileChecker(string filename)
+        {
+            bool valid = true;
+
+            using (var reader = new StreamReader(filename))
+            {
+                string line1 = reader.ReadLine();
+                while (reader.EndOfStream)
+                {
+                    string line2 = reader.ReadLine();
+                    if (string.Compare(line1, line2, StringComparison.Ordinal) < 0)
+                    {
+                        valid = false;
+                        break;
+                    }
+                    line1 = line2;
+                }
+            }
+
+            return valid;
+        }
+    }
+}
